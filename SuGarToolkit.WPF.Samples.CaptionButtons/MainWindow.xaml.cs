b4cfc248ffd948b1b610ad6b728fc35229525d59
Sky_SuGar <wxk0218@icloud.com>
2025-10-06 21:22:53 +0800

    Add WindowFullScreenHandler for sample app

--- SuGarToolkit.WPF.Samples.CaptionButtons/MainWindow.xaml.cs
+++ SuGarToolkit.WPF.Samples.CaptionButtons/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 
 namespace SuGarToolkit.WPF.Samples.CaptionButtons;
 
@@ -7,6 +8,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        _fullScreenHandler = new WindowFullScreenHandler(this);
     }
 
     private void Window_StateChanged(object sender, EventArgs e)
@@ -23,4 +25,36 @@ public partial class MainWindow : Window
                 break;
         }
     }
+
+    private void PaneToggleButtonClick(object sender, EventArgs e)
+    {
+        if (PART_SettingsScrollViewer.Visibility is Visibility.Visible)
+        {
+            PART_SettingsScrollViewer.Visibility = Visibility.Collapsed;
+        }
+        else
+        {
+            PART_SettingsScrollViewer.Visibility = Visibility.Visible;
+        }
+    }
+
+    private void HelpButtonClick(object sender, EventArgs e)
+    {
+        using Process process = new()
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                UseShellExecute = true,
+                FileName = @"https://github.com/SuGar0218/NativeLikeCaptionButton-WPF"
+            }
+        };
+        process.Start();
+    }
+
+    private void FullScreenButtonClick(object sender, RoutedEventArgs e)
+    {
+        _fullScreenHandler.ToggleFullScreen();
+    }
+
+    private readonly WindowFullScreenHandler _fullScreenHandler;
 }
---
+++ SuGarToolkit.WPF.Samples.CaptionButtons/WindowFullScreenHandler.cs
@@ -0,0 +1,94 @@
+﻿using System.Windows;
+using System.Windows.Shell;
+
+namespace SuGarToolkit.WPF.Samples.CaptionButtons;
+
+public class WindowFullScreenHandler
+{
+    public WindowFullScreenHandler(Window window)
+    {
+        _window = window;
+        _window.StateChanged += OnWindowStateChanged;
+    }
+
+    public void ToggleFullScreen()
+    {
+        if (IsFullScreen)
+        {
+            ExitFullScreen();
+        }
+        else
+        {
+            EnterFullScreen();
+        }
+    }
+
+    public void EnterFullScreen()
+    {
+        if (IsFullScreen)
+            return;
+
+        IsFullScreen = true;
+
+        _windowStyle = _window.WindowStyle;
+        _window.WindowStyle = WindowStyle.None;
+
+        _windowState = _window.WindowState;
+        _window.WindowState = WindowState.Minimized;
+        _window.WindowState = WindowState.Maximized;
+
+        WindowChrome windowChrome = WindowChrome.GetWindowChrome(_window);
+        if (windowChrome is not null)
+        {
+            _windowChrome = (WindowChrome) windowChrome.Clone();
+        }
+        WindowChrome.SetWindowChrome(_window, (WindowChrome) _fullScreenWindowChrome.Clone());
+    }
+
+    public void ExitFullScreen()
+    {
+        if (!IsFullScreen)
+            return;
+
+        IsFullScreen = false;
+
+        _window.WindowStyle = _windowStyle;
+        WindowChrome.SetWindowChrome(_window, _windowChrome);
+        _window.WindowState = _windowState;
+    }
+
+    public void ExitFullScreen(WindowState state)
+    {
+        if (!IsFullScreen)
+            return;
+
+        IsFullScreen = false;
+
+        _window.WindowStyle = _windowStyle;
+        WindowChrome.SetWindowChrome(_window, _windowChrome);
+        _window.WindowState = state;
+    }
+
+    private void OnWindowStateChanged(object? sender, EventArgs e)
+    {
+        if (_window.WindowState is WindowState.Normal)
+        {
+            ExitFullScreen(WindowState.Normal);
+        }
+    }
+
+    public bool IsFullScreen { get; private set; }
+
+    private readonly Window _window;
+    private WindowStyle _windowStyle;
+    private WindowState _windowState;
+    private WindowChrome? _windowChrome;
+
+    private static readonly WindowChrome _fullScreenWindowChrome = new()
+    {
+        GlassFrameThickness = new Thickness(double.Epsilon),
+        UseAeroCaptionButtons = false,
+        CaptionHeight = 0,
+        CornerRadius = new CornerRadius(0)
+    };
+}
